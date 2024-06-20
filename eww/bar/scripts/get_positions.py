6f09ee690a0a906d7bb7e370dcdf01fcb7c02ea2
Michal Vu <sa1g0n@tuta.io>
2024-06-20 20:01:53 +0200

    minor reformat

--- eww/bar/scripts/get_positions.py
+++ eww/bar/scripts/get_positions.py
@@ -1,11 +1,11 @@
 import subprocess
-import argparse
 import json
 from typing import List
 
 BATTERY_WIDTH = 240
 
-def get_monitor_resolution() -> List[int]:
+
+def get_scaled_resolution() -> List[int]:
     monitor_info = "hyprctl monitors -j | jq '.[0] | {width, height, scale}'"
     # Execute the command using subprocess.Popen
     process = subprocess.Popen(
@@ -23,12 +23,16 @@ def get_monitor_resolution() -> List[int]:
     y_res = results["height"] / monitor_scale
     return [x_res, y_res]
 
+
 def get_battery_coordinates() -> int:
-    monitor_resolution = get_monitor_resolution()
-    x_coordinate = int(monitor_resolution[0] - (monitor_resolution[0] * 0.6 / 2) - BATTERY_WIDTH)
+    monitor_resolution = get_scaled_resolution()
+    x_coordinate = int(
+        monitor_resolution[0] - (monitor_resolution[0] * 0.6 / 2) - BATTERY_WIDTH
+    )
     return x_coordinate
 
+
 def get_main_bar_width() -> int:
-    monitor_resolution = get_monitor_resolution()
+    monitor_resolution = get_scaled_resolution()
     bar_width = int(monitor_resolution[0] * 0.4)
     return bar_width
